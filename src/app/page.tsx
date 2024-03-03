7b125b93ef6c8fada4eacdc386bec1ef15909bf4
RataSurithpinyo <rataprim30026@gmail.com>
2024-03-03 13:42:16 +0700

    add gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,15 @@
 import Intro from "@/components/Intro";
 import Middle from "@/components/Middle";
 import { ThreeDCardDemo } from "@/components/ThreeD";
+import { BackgroundGradientAnimation } from "@/components/ui/background-gradient";
+import Gradient from "@/components/Gradient";
 export default function Example() {
   return (
     <>
       <Intro />
       <ThreeDCardDemo/>
       <Middle />
-      
+      <Gradient />
     </>
   );
 }
--- src/components/Middle.tsx
+++ src/components/Middle.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 const features = [
-    { name: 'Origin', description: 'Designed by Good Goods, Inc.' },
-    { name: 'Material', description: 'Solid walnut base with rare earth magnets and powder coated steel card cover' },
-    { name: 'Dimensions', description: '6.25" x 3.55" x 1.15"' },
-    { name: 'Finish', description: 'Hand sanded and finished with natural oil' },
-    { name: 'Includes', description: 'Wood card tray and 3 refill packs' },
-    { name: 'Considerations', description: 'Made from natural materials. Grain and color vary with each item.' },
+    { name: 'Software Development', description: 'Designed by Good Goods, Inc.' },
+    { name: 'UX/UI', description: 'Solid walnut base with rare earth magnets and powder coated steel card cover' },
+    { name: 'Software Architecture', description: '6.25" x 3.55" x 1.15"' },
+    { name: 'Cloud Computing', description: 'Hand sanded and finished with natural oil' },
   ]
 export default function Middle() {
   return (
@@ -15,11 +13,11 @@ export default function Middle() {
         <div>
           <h2 className="mt-6 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">My passion</h2>
           <p className="mt-4 text-gray-500">
-            The walnut wood card tray is precision milled to perfectly fit a stack of Focus cards. The powder coated
-            steel divider separates active cards from new ones, or can be used to archive important task lists.
+          I am passionate about web development and software engineering. Looking to contribute my software skills to a challenging role. With excitement, I am eager to learn more about both Frontend and Backend technologies. I am ready to take a deep dive into 
+          challenges in software development.
           </p>
 
-          <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
+          <dl className="mt-16 mb-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
             {features.map((feature) => (
               <div key={feature.name} className="border-t border-gray-200 pt-4">
                 <dt className="font-medium text-gray-900">{feature.name}</dt>
